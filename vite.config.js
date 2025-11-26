1819507c36651d837bf2aa93f719d7d6731a13ad
deepak <79254781+Tarun123-kittu@users.noreply.github.com>
2025-11-26 13:49:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { createHtmlPlugin } from "vite-plugin-html";
-import Sitemap from "vite-plugin-sitemap";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ViteSitemap } from 'vite-plugin-sitemap';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { viteSSG } from 'vite-ssg/serialized-data';
 
+const routes = [
+  { path: '/', name: 'Home' },
+  { path: '/intro', name: 'Intro' },
+  { path: '/quiz', name: 'Quiz' },
+  { path: '/plans/:phone/:id', name: 'Plan' },
+  { path: '/Top-Roth-Conversion-Retirement-Questions/:slug', name: 'Roth' },
+];
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    createHtmlPlugin({ minify: true }),
-    Sitemap({ hostname: "https://retiremate.com" }),
+    viteSSG({ includedRoutes: () => routes }),
+    ViteSitemap({
+      baseUrl: 'https://dev.retiremate.com',
+      routes,
+      generateRobotsTxt: true,
+    }),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'Default Title',
+          description: 'Default Description',
+        },
+      },
+    }),
   ],
-   server: {
-    host: false, 
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
   },
-})
+});
