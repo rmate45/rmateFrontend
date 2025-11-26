737af9bca6b7f86816a900dad5f3691f9b99655a
deepak <79254781+Tarun123-kittu@users.noreply.github.com>
2025-11-26 12:13:48 +0530

    seo added

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import Pages from "vite-plugin-pages";
 import { createHtmlPlugin } from "vite-plugin-html";
 import Sitemap from "vite-plugin-sitemap";
 
-const NGROK_HOST =  "jovanni-sighlike-nonoptimistically.ngrok-free.dev";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    Pages({ extensions: ["jsx", "js"] }),
     createHtmlPlugin({ minify: true }),
-    Sitemap({ hostname: "https://dev.retiremate.com" }),
+    Sitemap({ hostname: "https://retiremate.com" }),
   ],
    server: {
-    host: true, // listen on network interfaces
-    // allow this specific ngrok hostname (or use array of hostnames)
-    allowedHosts: [NGROK_HOST, ".ngrok-free.dev"],
-    // HMR settings so websocket connects from the public ngrok host
-    hmr: {
-      protocol: "wss",
-      host: NGROK_HOST,
-      port: 443,
-    },
+    host: false, 
   },
 })
