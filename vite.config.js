0e201cbb41916b0cd24bf6986d6a43d8bab8d601
tarun <tarun.gautam@ultivic.com>
2025-12-02 16:33:04 +0530

    dynamice image updated for meta and static url updated with env

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { createHtmlPlugin } from "vite-plugin-html";
-import Sitemap from "vite-plugin-sitemap";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import Sitemap from 'vite-plugin-sitemap';
 
+export default defineConfig(({ mode }) => {
+  // ✅ Correct: use project root path
+  const env = loadEnv(mode, process.cwd(), '');
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    createHtmlPlugin({ minify: true }),
-    Sitemap({ hostname: "https://dev.retiremate.com" }),
-  ],
-     server: {
-    historyApiFallback: true
-  }
-   
-})
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      createHtmlPlugin({ minify: true }),
+
+      Sitemap({
+        hostname: env.VITE_WEBSITE_URL || 'https://dev.retiremate.com',
+      }),
+    ],
+
+    build: {
+      sourcemap: false,
+    },
+  };
+});
